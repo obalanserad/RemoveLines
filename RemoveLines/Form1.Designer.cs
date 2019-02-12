1f5ac9f0d31cac35748c225676fdfb25a14b8d66
Anton Hell <antonirymden@hotmail.com>
2019-02-12 12:00:01 +0100

    All everything

---
+++ RemoveLines/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace RemoveLines
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxIn = new System.Windows.Forms.RichTextBox();
+            this.txtBoxOut = new System.Windows.Forms.RichTextBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBoxIn
+            // 
+            this.txtBoxIn.Location = new System.Drawing.Point(12, 12);
+            this.txtBoxIn.Name = "txtBoxIn";
+            this.txtBoxIn.Size = new System.Drawing.Size(435, 286);
+            this.txtBoxIn.TabIndex = 0;
+            this.txtBoxIn.Text = "";
+            this.txtBoxIn.TextChanged += new System.EventHandler(this.txtBoxIn_TextChanged);
+            // 
+            // txtBoxOut
+            // 
+            this.txtBoxOut.Location = new System.Drawing.Point(524, 12);
+            this.txtBoxOut.Name = "txtBoxOut";
+            this.txtBoxOut.Size = new System.Drawing.Size(435, 286);
+            this.txtBoxOut.TabIndex = 0;
+            this.txtBoxOut.Text = "";
+            this.txtBoxOut.TextChanged += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(372, 304);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 1;
+            this.btnRemove.Text = "Do it!";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(884, 304);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 1;
+            this.btnCopy.Text = "DO IT!";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(275, 307);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Remove stuff";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(812, 307);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Copy text";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(971, 435);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.txtBoxOut);
+            this.Controls.Add(this.txtBoxIn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtBoxIn;
+        private System.Windows.Forms.RichTextBox txtBoxOut;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ RemoveLines/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RemoveLines
+{
+    public partial class Form1 : Form
+    {
+        string oldText = "";
+        string newText = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtBoxIn_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            oldText = txtBoxIn.Text;
+            List<string> newWords = new List<string>();
+
+            string[] words = oldText.Split(' ');
+            foreach (string word in words)
+            {
+                if(word != "modified:" && word != "")
+                { 
+                newWords.Add(word.Trim());
+                }
+            }
+
+            newText = string.Join(" ", newWords.ToArray());
+            txtBoxOut.Text = "git add " + newText;
+
+        }
+
+        private void txtBoxOut_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if (txtBoxOut.Text != null)
+            { 
+                txtBoxOut.SelectAll();
+            txtBoxOut.Copy();
+            }
+        }
+    }
+}
