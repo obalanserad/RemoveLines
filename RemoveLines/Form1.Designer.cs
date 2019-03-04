145b0101d2ffad80bfe20565660c730ea10f0fb7
Anton Hell <antonirymden@hotmail.com>
2019-03-04 15:15:35 +0100

    beta

--- RemoveLines/Form1.Designer.cs
+++ RemoveLines/Form1.Designer.cs
@@ -31,6 +31,10 @@
             this.txtBoxIn = new System.Windows.Forms.RichTextBox();
             this.btnRemove = new System.Windows.Forms.Button();
             this.btnCopy = new System.Windows.Forms.Button();
+            this.addGitBox = new System.Windows.Forms.CheckBox();
+            this.rmvModBox = new System.Windows.Forms.CheckBox();
+            this.rmvDelBox = new System.Windows.Forms.CheckBox();
+            this.rmvLinebreakBox = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // txtBoxIn
@@ -38,7 +42,7 @@
             this.txtBoxIn.BackColor = System.Drawing.Color.Black;
             this.txtBoxIn.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.txtBoxIn.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBoxIn.ForeColor = System.Drawing.Color.Red;
+            this.txtBoxIn.ForeColor = System.Drawing.Color.Lime;
             this.txtBoxIn.Location = new System.Drawing.Point(9, 2);
             this.txtBoxIn.Margin = new System.Windows.Forms.Padding(2);
             this.txtBoxIn.Name = "txtBoxIn";
@@ -69,13 +73,61 @@
             this.btnCopy.UseVisualStyleBackColor = true;
             this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
             // 
+            // addGitBox
+            // 
+            this.addGitBox.AutoSize = true;
+            this.addGitBox.Location = new System.Drawing.Point(342, 13);
+            this.addGitBox.Name = "addGitBox";
+            this.addGitBox.Size = new System.Drawing.Size(93, 17);
+            this.addGitBox.TabIndex = 2;
+            this.addGitBox.Text = "Add \"git add \"";
+            this.addGitBox.UseVisualStyleBackColor = true;
+            this.addGitBox.CheckedChanged += new System.EventHandler(this.addGitBox_CheckedChanged);
+            // 
+            // rmvModBox
+            // 
+            this.rmvModBox.AutoSize = true;
+            this.rmvModBox.Location = new System.Drawing.Point(342, 37);
+            this.rmvModBox.Name = "rmvModBox";
+            this.rmvModBox.Size = new System.Drawing.Size(121, 17);
+            this.rmvModBox.TabIndex = 3;
+            this.rmvModBox.Text = "Remove \"modified:\"";
+            this.rmvModBox.UseVisualStyleBackColor = true;
+            this.rmvModBox.CheckedChanged += new System.EventHandler(this.rmvModBox_CheckedChanged);
+            // 
+            // rmvDelBox
+            // 
+            this.rmvDelBox.AutoSize = true;
+            this.rmvDelBox.Location = new System.Drawing.Point(342, 61);
+            this.rmvDelBox.Name = "rmvDelBox";
+            this.rmvDelBox.Size = new System.Drawing.Size(117, 17);
+            this.rmvDelBox.TabIndex = 4;
+            this.rmvDelBox.Text = "Remove \"deleted:\"";
+            this.rmvDelBox.UseVisualStyleBackColor = true;
+            this.rmvDelBox.CheckedChanged += new System.EventHandler(this.rmvDelBox_CheckedChanged);
+            // 
+            // rmvLinebreakBox
+            // 
+            this.rmvLinebreakBox.AutoSize = true;
+            this.rmvLinebreakBox.Location = new System.Drawing.Point(342, 85);
+            this.rmvLinebreakBox.Name = "rmvLinebreakBox";
+            this.rmvLinebreakBox.Size = new System.Drawing.Size(112, 17);
+            this.rmvLinebreakBox.TabIndex = 5;
+            this.rmvLinebreakBox.Text = "Remove linebreak";
+            this.rmvLinebreakBox.UseVisualStyleBackColor = true;
+            this.rmvLinebreakBox.CheckedChanged += new System.EventHandler(this.rmvLinebreakBox_CheckedChanged);
+            // 
             // Form1
             // 
             this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(343, 279);
+            this.ClientSize = new System.Drawing.Size(471, 279);
+            this.Controls.Add(this.rmvLinebreakBox);
+            this.Controls.Add(this.rmvDelBox);
+            this.Controls.Add(this.rmvModBox);
+            this.Controls.Add(this.addGitBox);
             this.Controls.Add(this.btnCopy);
             this.Controls.Add(this.btnRemove);
             this.Controls.Add(this.txtBoxIn);
@@ -84,6 +136,7 @@
             this.Text = "RemoveLines";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -92,6 +145,10 @@
         private System.Windows.Forms.RichTextBox txtBoxIn;
         private System.Windows.Forms.Button btnRemove;
         private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.CheckBox addGitBox;
+        private System.Windows.Forms.CheckBox rmvModBox;
+        private System.Windows.Forms.CheckBox rmvDelBox;
+        private System.Windows.Forms.CheckBox rmvLinebreakBox;
     }
 }
 
--- RemoveLines/Form1.cs
+++ RemoveLines/Form1.cs
@@ -13,8 +13,11 @@ namespace RemoveLines
 {
     public partial class Form1 : Form
     {
-        string oldText = "";
-        string newText = "";
+        string oldText;
+        string newText;
+        string addGit;
+        bool removeLinebreak;
+        string[] options= new string[2];
 
         public Form1()
         {
@@ -22,7 +25,20 @@ namespace RemoveLines
             btnCopy.Text = "Copy";
             btnRemove.Text = "Remove";
             CreateContextMenu();
-            //txtBoxIn.Text = Clipboard.GetText();
+            oldText = string.Empty;
+            newText = string.Empty;
+            addGit = string.Empty;
+            options[0] = string.Empty;
+            options[1] = string.Empty;
+            //options[2] = string.Empty;
+            removeLinebreak = false;
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
 
         private void CreateContextMenu()
@@ -41,12 +57,15 @@ namespace RemoveLines
             txtBoxIn.ContextMenu = contextMenu;
         }
 
-        private void txtBoxIn_TextChanged(object sender, EventArgs e)
+        //NOT IN USE ATM
+        private string NewLine(string input)
         {
-
+            if (input.Contains(Environment.NewLine))
+            {
+                input = input.Replace(Environment.NewLine, String.Empty);
+            }
+            return input;
         }
-
-
 
         private void btnRemove_Click(object sender, EventArgs e)
         {
@@ -56,38 +75,30 @@ namespace RemoveLines
             string[] words = oldText.Split(' ');
             foreach (string word in words)
             {
-                if(word != "modified:" && word != string.Empty && word != "deleted:")
-                { 
-                newWords.Add(word.Trim());
+                if(!options.Contains(word) && !string.IsNullOrEmpty(word))
+                {
+                    if(removeLinebreak == true)
+                        newWords.Add(NewLine(word).Trim());
+                    else
+                        newWords.Add(word.Trim());
                 }
             }
 
-            txtBoxIn.Text = string.Join(" ", newWords.ToArray());
             newText = string.Join(" ", newWords.ToArray());
-            txtBoxIn.Text = "git add " + newText;
+            txtBoxIn.Text = addGit + newText.TrimStart();
 
-        }
-
-        private void txtBoxOut_TextChanged(object sender, EventArgs e)
-        {
-            
         }
 
         private void btnCopy_Click(object sender, EventArgs e)
         {
-            if (txtBoxOut.Text != null)
+            if (txtBoxIn.Text != null)
             { 
-                txtBoxOut.SelectAll();
-            txtBoxOut.Copy();
+                txtBoxIn.SelectAll();
+            txtBoxIn.Copy();
             }
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-
+        //CUT COPY PASTE ACTIONS
         void CutAction(object sender, EventArgs e)
         {
             txtBoxIn.Cut();
@@ -95,39 +106,36 @@ namespace RemoveLines
 
         void CopyAction(object sender, EventArgs e)
         {
-            Graphics objGraphics;
-            Clipboard.SetData(DataFormats.Rtf, txtBoxIn.SelectedRtf);
-            Clipboard.Clear();
+            Clipboard.SetData(DataFormats.Text, txtBoxIn.Text);
+            //Clipboard.Clear();
         }
 
         void PasteAction(object sender, EventArgs e)
         {
             if (Clipboard.ContainsText(TextDataFormat.Text))
             {
-                txtBoxIn.Text
-                    = Clipboard.GetText();
+                txtBoxIn.Text = Clipboard.GetText();
             }
         }
 
-
-        private void txtBoxIn_MouseClick(object sender, MouseEventArgs e)
+        private void addGitBox_CheckedChanged(object sender, EventArgs e)
         {
-            //if (e.Button == System.Windows.Forms.MouseButtons.Right)
-            //{
-                //ContextMenu contextMenu = new System.Windows.Forms.ContextMenu();
-                //MenuItem menuItem = new MenuItem("Cut");
-                //menuItem.Click += new EventHandler(CutAction);
-                //contextMenu.MenuItems.Add(menuItem);
-                //menuItem = new MenuItem("Copy");
-                //menuItem.Click += new EventHandler(CopyAction);
-                //contextMenu.MenuItems.Add(menuItem);
-                //menuItem = new MenuItem("Paste");
-                //menuItem.Click += new EventHandler(PasteAction);
-                //contextMenu.MenuItems.Add(menuItem);
-
-                //richTextBox1.ContextMenu = contextMenu;
-            //}
+            addGit = "git add ";
         }
 
+        private void rmvModBox_CheckedChanged(object sender, EventArgs e)
+        {
+            options[0] = "modified:";
+        }
+
+        private void rmvDelBox_CheckedChanged(object sender, EventArgs e)
+        {
+            options[1] = "deleted:";
+        }
+
+        private void rmvLinebreakBox_CheckedChanged(object sender, EventArgs e)
+        {
+            removeLinebreak = true;
+        }
     }
 }
